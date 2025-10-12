a6231d0e785293550e077caed0a3c0bcab5d66dd
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-10-11 23:25:44 -0700

    fleshed out foundations for persona nd student classes

--- src/data_classes/Person.java
+++ src/data_classes/Person.java
@@ -2,4 +2,56 @@ package data_classes;
 
 public abstract class Person {
 
+	// person fields
+	private int id;
+	private String name;
+	
+	
+	// symbolic constants
+	
+	
+	
+	// constructor
+	public Person() {
+		super();
+	}
+	
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	
+	
+	// getters and setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	// abstrct methods
+	abstract String getDetails();
+	
+	
+	// toString
+	@Override
+	public String toString() {
+		return "Person "
+			 + "[id=" + id
+			 + ",name=" + name
+			 + "]";
+	}
+	
 }
--- src/data_classes/Student.java
+++ src/data_classes/Student.java
@@ -1,5 +1,62 @@
 package data_classes;
 
-public class Student {
+public class Student extends Person {
+	
+	// student fields
+	private String course;
+	private double gpa;
+	
+	
+	// symbolic constants
+	private static String default_course;
+	private static double default_gpa;
+	
+	
+	// initializer
+	{
+		default_course = "n/a";
+		default_gpa    = 0.0;
+	}
+	
+	
+	// constructors
+	public Student() {
+		super();
+	}
+	
+	
+	public Student(String course, double gpa) {
+		this.course = course;
+		this.gpa    = gpa;
+	}
+	
+
+	// getters and setters
+	public String getCourse() {
+		return course;
+	}
+
+	
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	
+	public double getGpa() {
+		return gpa;
+	}
+	
+	
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	
+    // toString
+	@Override
+	String getDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
