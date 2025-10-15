59f1cd7717398ca31ad51fa7e608962736898d91
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-10-14 21:37:36 -0700

    created validation methods for person class

--- src/data_classes/Person.java
+++ src/data_classes/Person.java
@@ -8,7 +8,17 @@ public abstract class Person {
 	
 	
 	// symbolic constants
+	private static int MIN_ID_VALUE;
+	private static int MAX_ID_VALUE;
+	private static int NAME_MAX_LENGTH;
 	
+	
+	// initializer
+	{
+		MIN_ID_VALUE    = 1000;
+		MAX_ID_VALUE    = 9999;
+		NAME_MAX_LENGTH = 30;
+	}
 	
 	
 	// constructor
@@ -17,21 +27,26 @@ public abstract class Person {
 	}
 	
 	public Person(int id, String name) {
+		validateID(id);
 		this.id = id;
+		validateName(name);
 		this.name = name;
 	}
 	
 	
 	
 	// validations
-	// TODO validation methods
 	private static void validateID(int id) {
-		
+		if(id < MIN_ID_VALUE || id > MAX_ID_VALUE) {
+			throw new IllegalArgumentException("Invalid id: " + id);
+		}
 	}
 	
-	private static void validateName(String name) {
-		
-	}
+    private static void validateName(String name) {
+        if (name.isBlank() || name == null || name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("Invalid name: " + name + ". Name must not be blank and must be less than 30 characters.");
+        }
+    }
 
 	
 	
@@ -41,6 +56,7 @@ public abstract class Person {
 	}
 
 	public void setId(int id) {
+		validateID(id);
 		this.id = id;
 	}
 
@@ -49,6 +65,7 @@ public abstract class Person {
 	}
 
 	public void setName(String name) {
+		validateName(name);
 		this.name = name;
 	}
 
