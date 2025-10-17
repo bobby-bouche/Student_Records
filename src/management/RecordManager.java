c5ee25f228bffcd7174e913c7c44b500eab75dbe
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-10-16 22:10:14 -0700

    fleshed out the foundations for recordManager class. All is needed now is fill in the 5 class speific methods

--- src/management/RecordManager.java
+++ src/management/RecordManager.java
@@ -1,5 +1,53 @@
 package management;
 
-public class RecordManager {
 
+import java.util.HashMap;
+import java.util.Map;
+
+import data_classes.Student;
+
+public class RecordManager {
+	
+	// fields
+	private static Map<Integer, Student> students;
+	
+	
+	
+	// constructor
+	public RecordManager() {
+		super();
+		students = new HashMap<>();
+	}
+
+
+
+	// getter method 
+	public static Map<Integer, Student> getStudents() {
+		return students;
+	}
+
+	
+	
+	
+	public void addStudent(Student student) {}
+	
+	
+	
+	public void removeStudent(int id) {}
+	
+	
+	
+	public void listAllStudents() {}
+	
+	
+	
+	public void saveAll() {}
+	
+	
+	
+	public void loadAll() {}
+	
+	
+	
+	
 }
